ce237a8b8b0056c3a7fd8325387d898e26ae0c76
JingyulGary <kingyulee.lj@gmail.com>
2019-03-06 13:55:00 -0800

    3/6 2pm

--- src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -75,4 +75,39 @@ public class ProjectTaskService {
 		return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
 	}
 
+	public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id) {
+		
+		//make sure we are searching on an existing backlog
+		Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
+		if(backlog == null) {
+			throw new ProjectNotFoundException("Project with ID '" + backlog_id + "' does not exist");
+		}
+		
+		//make sure that out task exists
+		ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+		if(projectTask == null) {
+			throw new ProjectNotFoundException("Project Task '" + pt_id + "' not found");
+		}
+		
+		//make sure we are searching on the right backlog
+		if(!projectTask.getBacklog().getProjectIdentifier().equals(backlog_id)) {
+			throw new ProjectNotFoundException("Project Task '" + pt_id + "' does not exist in project: '" + backlog_id);
+		}
+		
+		return projectTask;
+	}
+	
+	public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id) {
+		ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+		
+		projectTask = updatedTask;
+		
+		return projectTaskRepository.save(projectTask);
+	}
+	
+	public void deletePTByProjectSequence(String backlog_id, String pt_id) {
+		ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+		
+		projectTaskRepository.delete(projectTask);
+	}
 }
